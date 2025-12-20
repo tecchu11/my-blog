df9ddb718acebba94da480767ca8fff572a64d2a
tecchu11 <agdjmp.ajtt@gmail.com>
2025-12-20 22:57:43 +0900

    記事コンポーネントのスタイルを調整し、コードブロックの表示を改善

--- components/article.tsx
+++ components/article.tsx
@@ -9,7 +9,7 @@ interface ArticleProp {
 
 export function Article({ date, title, tags, children }: ArticleProp) {
     return (
-        <article className="flex flex-col max-w-full py-4 gap-10">
+        <article className="flex flex-col max-w-full py-4 gap-8">
             <div className="flex flex-col gap-4 pb-4 border-b-[0.5px]">
                 {date && (
                     <p className="text-slate-700 dark:text-slate-200">
